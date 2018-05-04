8dedce6337ceaef2e8f1dab2a59724629c6eacfb
turnt <frostmail@inbox.ru>
2018-05-05 00:31:04 +0300

    Исправлены ошибки линтинга

--- js/data.js
+++ js/data.js
@@ -61,30 +61,8 @@
     }
   };
 
-  // return random integer value from range
-  var getValueFromLimits = function (arr) {
-    var range = arr.slice();
-    var length = range.length;
-    var value = 100500; // by default for empty array
-
-    range.sort(window.util.sortAscendant);
-
-    if (length === 1) {
-      value = range[0];
-    } else if (length > 1) {
-      // addition is the random value from 0 to difference
-      // between last and first element
-      var addition = Math.floor(
-          Math.random() * (range[length - 1] - range[0])
-      );
-      value = range[0] + addition;
-    }
-
-    return value;
-  };
-
   // return generated advert
-  var generateAdvertItem = function (props, id, title, url) {
+  var generateAdvertItem = function (props, id, title) {
     var advert = {};
 
     advert.offer = {};
